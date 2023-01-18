fc030d86c9a07075c8b1fb02ef97d61fb5d6fbee
Sérgio Ribeiro <sfilipe_ribeiro@hotmail.com>
2023-01-18 21:35:26 +0000

    React Test1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Olá Sérgio!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende React Já!!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
